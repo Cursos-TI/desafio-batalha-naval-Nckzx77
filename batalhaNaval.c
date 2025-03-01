a0277426b737a992c9197df3559864a4ede22ebe
Nckzx77 <saulokayro41@gmail.com>
2025-03-01 12:43:45 +0000

    Trabalho Feito batalhaNaval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,132 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char letras[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'}; //letras das colunas
+    int tabuleiro[10][10] = {0}; //tabuleiro do jogo batalha naval
+    int fimDoCone = 0;
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+    //apresentação
+    printf("TABULEIRO DE BATALHA NAVAL\n");
+    printf("\n");
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+    //definição dos navios horinzotalmente
+    //alterar horinzotalmente significa alterar os segundos quadrados
+    tabuleiro[2][2]= 3;
+    tabuleiro[2][3]= 3;
+    tabuleiro[2][4]= 3;
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+    //definição dos navios verticalmente
+    //alterar verticalmente significa alterar os primeiros quadrados
+    tabuleiro[5][8]= 3;
+    tabuleiro[6][8]= 3;
+    tabuleiro[7][8]= 3;
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    //definição dos navios na diagonal
+    //diagonal primária
+    for (int i = 0; i < 10; i++)//declarando externo            
+    {
+        for (int j = 4; j < 7; j++)//declarando interno. O valor de J indica onde a diagonal segue
+        {
+            if (i == j )
+            {
+                tabuleiro[i][j] = 3;
+            }
+        }
+    }
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    //diagonal secundária
+    for (int i = 0; i < 10; i++) //declarando externo
+    {
+        for (int j = 0; j < 3; j++) //declarando interno. O valor de J indica onde a diagonal segue
+        {
+            int soma = i + j; 
+            if (soma == 9)
+            {
+                tabuleiro[i][j] = 3;
+            }
+        }
+    }
 
+    //Habilidades especiais. Habilidades tem o valor 5
+    //habilidade do cone
+    for (int i = 0; i < 10; i++) //declarando externo
+    {
+        for (int j = 0; j < 10; j++) //declarando interno.
+        {
+            if (i == 4 && i * j == 8)
+            {
+                tabuleiro[i][j] = 5; // primeira camada
+            }
+
+            if (i == 5 && i * j <= 15 && j != 0) 
+            {
+                tabuleiro[i][j] = 5; //segunda camada
+            }
+
+            if (i == 6 && i * j <= 24)
+            {
+                tabuleiro[i][j] = 5; //tericeira camada
+            }
+    
+        }
+    }
+
+    //habilidade da cruz
+    for (int i = 0; i < 10; i++) //declarando externo
+    {
+        for (int j = 0; j < 10; j++) //declarando interno.
+        {
+            if (j == 7 && i < 3)
+            {
+                tabuleiro[i][j] = 5; //parte vertical
+            }
+
+            if (i == 1 && j >= 5)
+            {
+                tabuleiro[i][j] = 5; //parte horizontal
+            }
+        }
+    }
+
+    //habilidade do octaedro
+     for (int i = 0; i < 10; i++) //declarando externo
+    {
+        for (int j = 0; j < 10; j++) //declarando interno.
+        {
+            if (j == 5 && i >= 7)
+            {
+                tabuleiro[i][j] = 5;
+            }
+
+            if (i == 8 && j > 3 && j < 7)
+            {
+                tabuleiro[i][j] = 5;
+            }
+        }
+    }
+    
+    
+    //loops
+    //loop aninhados para exibir as letras das colunas
+    printf("   ");
+    for (int i = 0; i < 10; i++){
+        printf("%c ", letras[i]); //imprimindo as colunas
+    }
+    printf("\n");
+    
+    //loop aninhados para exibir tabuleiro
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d  ", i); //mostrar as linhas
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    printf("\n");
+   
     return 0;
 }
